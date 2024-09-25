8c3ddc4bc4f1f3b8cb5db2c7f9e77eb0cb321fc2
Isiah Chillous <44874147+ichillous@users.noreply.github.com>
2024-09-25 16:08:57 -0400

    test

---
+++ src/main/java/com/tms/demo/model/Task.java
@@ -0,0 +1,16 @@
+package com.tms.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data // All in one annotation
+@Table(name = "Tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+}
---
+++ src/main/java/com/tms/demo/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.tms.demo.repository;
+
+import com.tms.demo.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
