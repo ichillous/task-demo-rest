2ac42ece79bb3d0c1df36d19509a1246ed83c26f
Isiah Chillous <44874147+ichillous@users.noreply.github.com>
2024-09-27 14:37:11 -0400

    test

--- src/main/java/com/tms/demo/TaskManagementSystemApplication.java
+++ src/main/java/com/tms/demo/TaskManagementSystemApplication.java
@@ -9,5 +9,7 @@ public class TaskManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagementSystemApplication.class, args);
 	}
+	
+
 
 }
--- src/main/java/com/tms/demo/controller/TaskController.java
+++ src/main/java/com/tms/demo/controller/TaskController.java
@@ -2,6 +2,11 @@ package com.tms.demo.controller;
 
 import com.tms.demo.model.Task;
 import com.tms.demo.service.TaskService;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Controller
 @RequestMapping("/api/tasks")
 public class TaskController {
@@ -18,6 +24,7 @@ public class TaskController {
 
     @GetMapping
     public String getAllTasks(Model model) {
+        log.info("Fetching all Tasks");
         List<Task> tasks = taskService.getAllTasks();
         model.addAttribute("tasks", tasks);
         return "task-list";
@@ -25,6 +32,7 @@ public class TaskController {
 
     @GetMapping("/new")
     public String showCreateTaskForm(Model model) {
+        log.info("Displaying Task creation form");
         Task tasks = new Task();
         model.addAttribute("task", tasks);
         return "task-form";
@@ -32,10 +40,13 @@ public class TaskController {
 
     @GetMapping("/edit/{id}")
     public String showEditTaskForm(@PathVariable Long id, Model model) {
+        log.info("Fetching task with ID: {}", id);
         Optional<Task> optionalTask = taskService.getTaskById(id);
         if (optionalTask.isPresent()) {
             model.addAttribute("task", optionalTask.get());
+            log.info("Task found: {}", optionalTask.get());
         } else {
+            log.warn("Task with ID: {} not found", id);
             return "redirect:/api/tasks";
         }
         return "task-form";
@@ -45,8 +56,10 @@ public class TaskController {
     public String saveTask(@ModelAttribute("task") Task task) {
         if (task.getId() != null) {
             taskService.updateTask(task.getId(), task);
+            log.info("Creating new Task: {}", task);
         } else {
             taskService.createTask(task);
+            log.info("Updating Task: {}", task);
         }
         return "redirect:/api/tasks";
     }
@@ -54,6 +67,7 @@ public class TaskController {
 
     @GetMapping("/delete/{id}")
     public String deleteTask(@PathVariable Long id) {
+        log.info("Deleting Task with ID: {}", id);
         taskService.deleteTask(id);
         return "redirect:/api/tasks";
     }
